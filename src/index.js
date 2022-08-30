a1be853243741f81eb27cb62d9f43e13363b5b8c
Josiah Brown <josiahbrown321@gmail.com>
2022-08-30 10:14:42 -0500

    Commit before deploy 8

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { CartProvider } from "./context/CartContext";
 import { ProductsProvider } from "./context/ProductsContext";
 
-const root = createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <ProductsProvider>
